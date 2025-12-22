861609618155b7b41133b181aad6446dd35d8908
Minmoose <KennethBohr@outlook.com>
2025-12-22 09:22:08 -0600

    Ok, Don't look at the other commits, brain is waking up
    moodle

--- Moodles/GameHelpers/CharaWatcher.cs
+++ Moodles/GameHelpers/CharaWatcher.cs
@@ -148,9 +148,9 @@ public unsafe class CharaWatcher : IDisposable
     private void AddToWatcher(Character* chara)
     {
         if (chara is null
-            && chara->ObjectIndex >= 0 && chara->ObjectIndex <= 200
-            && chara->IsCharacter() == false
-            && chara->GetObjectKind() is not ObjectKind.Pc)
+            || chara->ObjectIndex < 0 || chara->ObjectIndex >= 200
+            || chara->IsCharacter() == false
+            || chara->GetObjectKind() is not ObjectKind.Pc)
             return;
 
         if (Rendered.Add((nint)chara))
